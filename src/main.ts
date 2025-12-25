8ba3794d30ad36c8ff32adae02714b626d1a45e8
= <=>
2025-12-25 11:07:09 +0530

    fix 2

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Server } from 'http';
-import express from "express";
+import express from 'express';
 import { ExpressAdapter } from '@nestjs/platform-express';
 
 let cachedServer: Server;
@@ -11,6 +11,7 @@ export const handler = async (): Promise<Server> => {
   if (!cachedServer) {
     const expressApp = express();
     const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+
     app.useGlobalPipes(
       new ValidationPipe({
         whitelist: true,
@@ -18,14 +19,17 @@ export const handler = async (): Promise<Server> => {
         transform: true,
       }),
     );
+
     await app.init();
     cachedServer = expressApp.listen();
   }
+
   return cachedServer;
 };
 
-
 async function bootstrap() {
+  if (process.env.VERCEL) return; 
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
@@ -34,6 +38,10 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`🚀 App running locally on http://localhost:${port}`);
 }
+
 bootstrap();
